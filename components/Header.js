fe5b5366d04497ca1b4d157c65922366569fa209
KelliePetersen <kelliepetersen@outlook.com>
2020-08-14 14:36:00 +1000

    add search input and sign up button

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,53 @@
 import Link from 'next/link';
-import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import { makeStyles, fade } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, Button, InputBase } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
+  heading: {
+    display: 'inline'
+  },
   toolbar: {
-    justifyContent: "space-between"
+    justifyContent: 'space-between'
+  },
+  search: {
+    display: 'inline-block',
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
   }
 }));
 
@@ -15,10 +58,28 @@ export default function Header() {
     <div>
       <AppBar position="fixed">
         <Toolbar className={classes.toolbar}>
-          <Typography variant="h6">
-            <Link href="/"><a>Stockbets</a></Link>
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <div>
+            <Typography variant="h6" className={classes.heading}>
+              <Link href="/"><a>Stockbets</a></Link>
+            </Typography>
+            <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          </div>
+          <div>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Sign Up</Button>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
