72888bd07e2285f2c647c3f3bb4ab446e4fe065f
KelliePetersen <kelliepetersen@outlook.com>
2020-08-14 19:52:21 +1000

    move search input box to separate component

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import SearchIcon from '@material-ui/icons/Search';
-import { AppBar, Toolbar, Typography, Button, InputBase } from '@material-ui/core';
+import SearchInput from './SearchInput';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 import { makeStyles, fade } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -14,45 +14,6 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: 'white',
     color: theme.palette.primary.main,
     marginLeft: theme.spacing(1)
-  },
-  search: {
-    display: 'inline-block',
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
   }
 }));
 
@@ -67,19 +28,7 @@ const Header = () => {
             <Typography variant="h6" className={classes.heading}>
               <Link href="/"><a>Stockbets</a></Link>
             </Typography>
-            <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </div>
+            <SearchInput />
           </div>
           <div>
             <Button variant="outlined" color="inherit">Login</Button>
