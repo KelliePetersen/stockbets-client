deaecc93ea8ae39431f2f4a3d7506dacb7063c30
KelliePetersen <kelliepetersen@outlook.com>
2020-08-15 15:29:49 +1000

    fetch stock data and display basic information on page

--- pages/stock/[id]/index.js
+++ pages/stock/[id]/index.js
@@ -3,32 +3,41 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import Layout from "../../../components/Layout";
 
-const VariablePage = ({title, body}) => {
+const VariablePage = (post) => {
   const router = useRouter();
   const {id} = router.query;
 
   return (
     <div>
       <Head>
-        <title>{title}</title>
+        <title>{post.symbol}: {post.companyName}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <h1>{title}</h1> 
-        <p>This is the variable page at URL: {id}</p>
-        <p>{body}</p>
-        <Link href="/">
-          <a>Go back home</a>
-        </Link>
+        <main style={{padding: '100px 0'}}>
+          <h1>{post.symbol}: ${post.latestPrice}</h1> 
+          <p>{post.companyName}</p>
+          <Link href="/">
+            <a>Go back home</a>
+          </Link>
+        </main>
       </Layout>
     </div>
   )
 }
 
 VariablePage.getInitialProps = async ({query}) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${query.id}`);
+  const res = await fetch(`https://sandbox.iexapis.com/stable/stock/${query.id}/quote?token=Tpk_d6cb491dc2a54a79a74012c3d4564673`);
   const post = res.json();
   return post;
 }
+
+//KEYS
+// symbol, companyName, primaryExchange, calculationPrice, open, openTime, openSource, close, closeTime, closeSource, high, highTime, 
+// highSource, low, lowTime, lowSource, latestPrice, latestSource, latestTime, latestUpdate, latestVolume, iexRealtimePrice, 
+// iexRealtimeSize, iexLastUpdated, delayedPrice, delayedPriceTime, oddLotDelayedPrice, oddLotDelayedPriceTime, extendedPrice, 
+// extendedChange, extendedChangePercent, extendedPriceTime, previousClose, previousVolume, change, changePercent, volume, 
+// iexMarketPercent, iexVolume, avgTotalVolume, iexBidPrice, iexBidSize, iexAskPrice, iexAskSize, iexOpen, iexOpenTime, iexClose, 
+// iexCloseTime, marketCap, peRatio, week52High, week52Low, ytdChange, lastTradeTime, isUSMarketOpen
 
 export default VariablePage;
