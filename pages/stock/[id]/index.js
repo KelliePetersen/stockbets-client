85ddcc23b6f3cf450a2b197880e450561fac4f52
KelliePetersen <kelliepetersen@outlook.com>
2020-08-15 17:44:56 +1000

    fetch multiple requests from api for stock info and intraday prices

--- pages/stock/[id]/index.js
+++ pages/stock/[id]/index.js
@@ -3,20 +3,20 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import Layout from "../../../components/Layout";
 
-const VariablePage = (post) => {
+const VariablePage = (stockData, priceData) => {
   const router = useRouter();
   const {id} = router.query;
 
   return (
     <div>
       <Head>
-        <title>{post.symbol}: {post.companyName}</title>
+        <title>{stockData.symbol}: {stockData.companyName}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
         <main style={{padding: '100px 0'}}>
-          <h1>{post.symbol}: ${post.latestPrice}</h1> 
-          <p>{post.companyName}</p>
+          <h1>{stockData.symbol}: ${stockData.latestPrice}</h1> 
+          <p>{stockData.companyName}</p>
           <Link href="/">
             <a>Go back home</a>
           </Link>
@@ -27,12 +27,19 @@ const VariablePage = (post) => {
 }
 
 VariablePage.getInitialProps = async ({query}) => {
-  const res = await fetch(`https://sandbox.iexapis.com/stable/stock/${query.id}/quote?token=Tpk_d6cb491dc2a54a79a74012c3d4564673`);
-  const post = res.json();
-  return post;
+  // const res = await fetch(`https://sandbox.iexapis.com/stable/stock/${query.id}/quote?token=Tpk_d6cb491dc2a54a79a74012c3d4564673`);
+  // const post = res.json();
+  // return post;
+
+  const [stockData, priceData] = await Promise.all([
+    fetch(`https://sandbox.iexapis.com/stable/stock/${query.id}/quote?token=Tpk_d6cb491dc2a54a79a74012c3d4564673`).then(r => r.json()),
+    fetch(`https://sandbox.iexapis.com/stable/stock/${query.id}/intraday-prices?token=Tpk_d6cb491dc2a54a79a74012c3d4564673`).then(r => r.json())
+  ]);
+
+  return { stockData, priceData };
 }
 
-//KEYS
+//KEYS /stock/{symbol}/quote
 // symbol, companyName, primaryExchange, calculationPrice, open, openTime, openSource, close, closeTime, closeSource, high, highTime, 
 // highSource, low, lowTime, lowSource, latestPrice, latestSource, latestTime, latestUpdate, latestVolume, iexRealtimePrice, 
 // iexRealtimeSize, iexLastUpdated, delayedPrice, delayedPriceTime, oddLotDelayedPrice, oddLotDelayedPriceTime, extendedPrice, 
