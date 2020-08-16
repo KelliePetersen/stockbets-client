c2f6d76bc94dee9c956f8a264efacd38d665bc0b
KelliePetersen <kelliepetersen@outlook.com>
2020-08-16 16:51:02 +1000

    add styling to chart

--- pages/chart.js
+++ pages/chart.js
@@ -1,16 +1,30 @@
 import { useRef, useEffect, useState } from 'react';
 import { select, line, axisBottom, axisRight, scaleLinear } from 'd3';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  svg: {
+    background: '#eee',
+    overflow: 'visible',
+    margin: '20px',
+    width: '90vw',
+    maxWidth: '800px',
+    height: '90vw',
+    maxHeight: '400px'
+  }
+}));
 
 const Chart = () => {
   const [data, setData] = useState([25, 30, 45, 60, 15, 30, 75, 100]);
+  const classes = useStyles();
   const svgRef = useRef();
 
   useEffect(() => {
     const svg = select(svgRef.current);
-    const xScale = scaleLinear().domain([0, data.length - 1]).range([0, 300]);
+    const xScale = scaleLinear().domain([0, data.length - 1]).range([0, 500]);
     const yScale = scaleLinear().domain([0, 100]).range([150, 0]);
 
-    const xAxis = axisBottom(xScale);
+    const xAxis = axisBottom(xScale).ticks(data.length).tickFormat(index => index + 1);
     svg.select('.x-axis').style('transform', 'translateY(150px)').call(xAxis);
 
     const yAxis = axisRight(yScale);
@@ -33,7 +47,7 @@ const Chart = () => {
 
   return (
     <>
-      <svg ref={svgRef} style={{background: '#eee', overflow: 'visible', margin: '20px'}}>
+      <svg ref={svgRef} className={classes.svg}>
         <g className="x-axis" />
         <g className="y-axis" />
       </svg>
