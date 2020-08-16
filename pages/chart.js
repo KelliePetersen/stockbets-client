550332d4c7a60f75d33ed8379294ef4747977fda
KelliePetersen <kelliepetersen@outlook.com>
2020-08-16 17:47:31 +1000

    use ResizeObserver to make chart responsive

--- pages/chart.js
+++ pages/chart.js
@@ -3,26 +3,53 @@ import { select, line, axisBottom, axisRight, scaleLinear } from 'd3';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  svg: {
-    background: '#eee',
-    overflow: 'visible',
-    margin: '20px',
+  wrapper: {
     width: '90vw',
     maxWidth: '800px',
     height: '90vw',
-    maxHeight: '400px'
+    maxHeight: '400px',
+    margin: '20px auto 0',
+    padding: '0 20px 20px 0'
+  },
+  svg: {
+    background: '#eee',
+    overflow: 'visible',
+    width: '100%',
+    height: '100%'
   }
 }));
+
+const useResizeObserver = (ref) => {
+  const [dimensions, setDimensions] = useState(null);
+  useEffect(() => {
+    const observeTarget = ref.current;
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach(entry => {
+        setDimensions(entry.contentRect);
+      })
+    })
+    resizeObserver.observe(observeTarget);
+    return () => {
+      resizeObserver.unobserve(observeTarget);
+    }
+  }, [ref]);
+  return dimensions;
+}
 
 const Chart = () => {
   const [data, setData] = useState([25, 30, 45, 60, 15, 30, 75, 100]);
   const classes = useStyles();
+  const wrapperRef = useRef();
   const svgRef = useRef();
+  const dimensions = useResizeObserver(wrapperRef);
 
   useEffect(() => {
     const svg = select(svgRef.current);
-    const xScale = scaleLinear().domain([0, data.length - 1]).range([0, 800]);
-    const yScale = scaleLinear().domain([0, 100]).range([400, 0]);
+
+    if (!dimensions) return;
+
+    const xScale = scaleLinear().domain([0, data.length - 1]).range([0, dimensions.width]);
+    const yScale = scaleLinear().domain([0, 100]).range([dimensions.height, 0]);
 
     const xAxis = axisBottom(xScale).ticks(data.length).tickFormat(index => index + 1);
     svg.select('.x-axis').style('transform', 'translateY(100%)').call(xAxis);
@@ -43,15 +70,15 @@ const Chart = () => {
       .attr('fill', 'none')
       .attr('stroke', 'blue');
 
-  }, [data]);
+  }, [data, dimensions]);
 
   return (
-    <>
+    <div ref={wrapperRef} className={classes.wrapper}>
       <svg ref={svgRef} className={classes.svg}>
         <g className="x-axis" />
         <g className="y-axis" />
       </svg>
-    </>
+    </div>
   )
 }
 
