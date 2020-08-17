69c498008d5f07050287d06b0098b3e03c143dd2
KelliePetersen <kelliepetersen@outlook.com>
2020-08-17 20:37:58 +1000

    create comment component

---
+++ components/Comment.js
@@ -0,0 +1,26 @@
+import faker from 'faker';
+
+const Comment = (props) => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.avatar()} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          {props.author}
+        </a>
+        <div className="metadata">
+          <span className="date">
+            {props.timeAgo}
+          </span>
+        </div>
+        <div className="text">
+          {faker.lorem.sentence()}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Comment;
