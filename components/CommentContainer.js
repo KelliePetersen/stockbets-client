7e7d43f1ab4e7a047aa2cb5890beac67c604d257
KelliePetersen <kelliepetersen@outlook.com>
2020-08-19 14:20:33 +1000

    add input section to submit comment

--- components/CommentContainer.js
+++ components/CommentContainer.js
@@ -1,19 +1,40 @@
-import { Divider } from "@material-ui/core";
+import { Divider, Avatar, Grid } from "@material-ui/core";
+import faker from 'faker';
 import Comment from './Comment';
 
 const CommentContainer = () => {
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div style={{marginTop: '80px'}}>
-      <h2 style={{marginBottom: '50px'}}>Comments</h2>
-      <Comment author="Jane" timeAgo="Today at 7:00pm"/>
-      <Divider style={{margin:'20px 0'}} />
-      <Comment author="David" timeAgo="Today at 3:00pm"/>
-      <Divider style={{margin:'20px 0'}} />
-      <Comment author="Bob" timeAgo="Today at 2:00pm"/>
-      <Divider style={{margin:'20px 0'}} />
-      <Comment author="Sarah" timeAgo="Today at 1:00pm"/>
-      <Divider style={{margin:'20px 0'}} />
-      <Comment author="Lynda" timeAgo="Today at 12:00pm"/>
+       
+      <Grid container wrap="nowrap" spacing={2}>
+        <Grid item>
+          <Avatar alt={`avatar of ${faker.name.findName()}`} src={faker.image.avatar()} style={{height: '70px', width: '70px'}} />
+        </Grid>
+        <Grid item xs zeroMinWidth>
+          <form className="commentForm" onSubmit={handleFormSubmit}>
+            <textarea rows="4" placeholder="Write your prediction here..." style={{width: '100%'}} />
+            <button type="submit" className="">Comment</button>
+          </form>
+        </Grid>
+      </Grid>
+
+      <div style={{marginTop: '50px'}}>
+        <h2 style={{marginBottom: '50px'}}>Predictions</h2>
+        <Comment author="Jane" timeAgo="Today at 7:00pm"/>
+        <Divider style={{margin:'20px 0'}} />
+        <Comment author="David" timeAgo="Today at 3:00pm"/>
+        <Divider style={{margin:'20px 0'}} />
+        <Comment author="Bob" timeAgo="Today at 2:00pm"/>
+        <Divider style={{margin:'20px 0'}} />
+        <Comment author="Sarah" timeAgo="Today at 1:00pm"/>
+        <Divider style={{margin:'20px 0'}} />
+        <Comment author="Lynda" timeAgo="Today at 12:00pm"/>
+      </div>
     </div>
   )
 }
