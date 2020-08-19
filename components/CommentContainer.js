5bb2aee19f6f9b60d5548db437efb1860bb026c6
KelliePetersen <kelliepetersen@outlook.com>
2020-08-19 14:57:18 +1000

    style comment form textarea

--- components/CommentContainer.js
+++ components/CommentContainer.js
@@ -1,8 +1,24 @@
 import { Divider, Avatar, Grid } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
 import faker from 'faker';
 import Comment from './Comment';
 
+const useStyles = makeStyles((theme) => ({
+  commentInput: {
+    width: '100%',
+    height: '80px',
+    border: 'none',
+    borderRadius: '20px',
+    boxShadow: '0 0 8px rgba(0,0,0,0.2)',
+    padding: '20px',
+    resize: 'vertical',
+    fontFamily: 'Arial',
+    fontSize: '0.875rem'
+  }
+}));
+
 const CommentContainer = () => {
+  const classes = useStyles();
   const dateFormat = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
   const handleFormSubmit = (e) => {
@@ -14,11 +30,11 @@ const CommentContainer = () => {
        
       <Grid container wrap="nowrap" spacing={2}>
         <Grid item>
-          <Avatar alt={`avatar of ${faker.name.findName()}`} src={faker.image.avatar()} style={{height: '70px', width: '70px'}} />
+          <Avatar alt={`avatar of ${faker.name.findName()}`} src={faker.image.avatar()} style={{height: '80px', width: '80px'}} />
         </Grid>
         <Grid item xs zeroMinWidth>
           <form className="commentForm" onSubmit={handleFormSubmit}>
-            <textarea rows="4" placeholder="Write your prediction here..." style={{width: '100%'}} />
+            <textarea name="content" placeholder="Write your prediction here..." className={classes.commentInput} />
             <button type="submit" className="">Comment</button>
           </form>
         </Grid>
@@ -26,15 +42,40 @@ const CommentContainer = () => {
 
       <div style={{marginTop: '50px'}}>
         <h2 style={{marginBottom: '50px'}}>Predictions</h2>
-        <Comment author={faker.name.findName()} avatar={faker.image.avatar()} content={faker.lorem.paragraph()} timeAgo={faker.date.past().toLocaleString("en-US", dateFormat)} />
+        <Comment 
+          author={faker.name.findName()} 
+          avatar={faker.image.avatar()} 
+          content={faker.lorem.paragraph()} 
+          timeAgo={faker.date.past().toLocaleString("en-US", dateFormat)} 
+        />
         <Divider style={{margin:'20px 0'}} />
-        <Comment author={faker.name.findName()} avatar={faker.image.avatar()} content={faker.lorem.paragraph()} timeAgo={faker.date.past().toLocaleString("en-US", dateFormat)} />
+        <Comment 
+          author={faker.name.findName()} 
+          avatar={faker.image.avatar()} 
+          content={faker.lorem.paragraph()} 
+          timeAgo={faker.date.past().toLocaleString("en-US", dateFormat)} 
+        />
         <Divider style={{margin:'20px 0'}} />
-        <Comment author={faker.name.findName()} avatar={faker.image.avatar()} content={faker.lorem.paragraph()} timeAgo={faker.date.past().toLocaleString("en-US", dateFormat)} />
+        <Comment 
+          author={faker.name.findName()} 
+          avatar={faker.image.avatar()} 
+          content={faker.lorem.paragraph()} 
+          timeAgo={faker.date.past().toLocaleString("en-US", dateFormat)} 
+        />
         <Divider style={{margin:'20px 0'}} />
-        <Comment author={faker.name.findName()} avatar={faker.image.avatar()} content={faker.lorem.paragraph()} timeAgo={faker.date.past().toLocaleString("en-US", dateFormat)} />
+        <Comment 
+          author={faker.name.findName()} 
+          avatar={faker.image.avatar()} 
+          content={faker.lorem.paragraph()} 
+          timeAgo={faker.date.past().toLocaleString("en-US", dateFormat)} 
+        />
         <Divider style={{margin:'20px 0'}} />
-        <Comment author={faker.name.findName()} avatar={faker.image.avatar()} content={faker.lorem.paragraph()} timeAgo={faker.date.past().toLocaleString("en-US", dateFormat)} />
+        <Comment 
+          author={faker.name.findName()} 
+          avatar={faker.image.avatar()} 
+          content={faker.lorem.paragraph()} 
+          timeAgo={faker.date.past().toLocaleString("en-US", dateFormat)} 
+        />
       </div>
     </div>
   )
